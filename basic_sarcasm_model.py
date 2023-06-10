70150abb19c99fa027f5a000235fa77c5e89b3da
Kalamojo <kojo22444488888888@gmail.com>
2023-06-10 12:06:21 -0400

    First Commit

---
+++ basic_sarcasm_model.py
@@ -0,0 +1,171 @@
+import numpy as np
+import random
+import pickle
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import pad_sequences, to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, Dense, Flatten
+
+padd_max = 100
+
+def filt(line, q):
+    ind = line.find(q)
+    if ind != -1:
+        return line[:line.find(q)]
+    return line
+
+sarcastic = []
+ind = 0
+with open('reddit_generated_sarcastic.txt', 'r', encoding="utf8") as f:
+    for line in f:
+        line = bytes(line, 'utf-8').decode('utf-8', 'ignore')[:-1]
+        if line[:7] == "Title: ":
+            sarcastic.append([line[7:]])
+        elif line[:9] == "Comment: ":
+            line = line[9:]
+            line = filt(line, "/s")
+            line = filt(line, "#s")
+            line = filt(line, "Sarcas")
+            line = filt(line, "sarcas")
+            sarcastic[ind].append(line)
+            ind += 1
+
+sar_len = len(sarcastic)
+
+with open('reddit_wild_sincere.txt', 'r', encoding="utf8") as f:
+    serious_org = [name.rstrip() for name in f.read().split('--------------------')][:-1]
+
+serious_list1 = random.sample(serious_org, sar_len)
+serious_list = serious_list1.copy()
+
+for i in range(len(serious_list)):
+    if serious_list[i][:1] == '\n':
+        serious_list[i] = serious_list[i][1:]
+    serious_list[i] = serious_list[i].split('\n---\n')
+ser_len = len(serious_list)
+
+sarcastic = np.array(sarcastic)
+serious = np.array(serious_list)
+
+docs = sarcastic.flatten().tolist() + serious.flatten().tolist()
+
+labels = [1] * sar_len + [0] * ser_len
+labels = np.array(labels)
+
+token = Tokenizer()
+token.fit_on_texts(docs)
+
+# saving
+with open('Basic_tokenizer.pickle', 'wb') as handle:
+    pickle.dump(token, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+sar_temp1 = pad_sequences(token.texts_to_sequences(sarcastic[:, 0]), maxlen=padd_max)
+sar_temp2 = pad_sequences(token.texts_to_sequences(sarcastic[:, 1]), maxlen=padd_max)
+sarcastic_num = np.column_stack((sar_temp1, sar_temp2)).reshape(sar_len, 2, padd_max)
+
+ser_temp1 = pad_sequences(token.texts_to_sequences(serious[:, 0]), maxlen=padd_max)
+ser_temp2 = pad_sequences(token.texts_to_sequences(serious[:, 1]), maxlen=padd_max)
+serious_num = np.column_stack((ser_temp1, ser_temp2)).reshape(ser_len, 2, padd_max)
+
+docs_x = np.concatenate((sarcastic_num, serious_num), axis=0)
+#print(docs_x)
+
+
+x_train, x_test, y_train, y_test = train_test_split(docs_x, labels, test_size=0.1, random_state=42)
+#print(x_train)
+#print(x_train.shape)
+
+word_size = len(token.word_index) + 1
+print("Vocabulary Size:", word_size)
+
+model = Sequential()
+model.add(Embedding(word_size, 12, input_shape=(2, padd_max)))
+model.add(Flatten())
+model.add(Dense(1, activation='sigmoid'))
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
+history = model.fit(x_train, y_train, batch_size=128, epochs=20, validation_split=0.1)
+
+print("\nSame-distribution test accuracy:", model.evaluate(x_test, y_test)[1])
+from sklearn.metrics import classification_report
+
+predict_x=model.predict(x_test)
+classes_x=np.round(predict_x)
+print(classification_report(y_test, classes_x))
+
+
+model.save("Basic_model.h5")
+
+#############
+# Testing Set
+#############
+
+with open('reddit_wild_sarcastic.txt', 'r', encoding="utf8") as f:
+    sarcastic = [filt(name, "/s").rstrip() for name in f.read().split('--------------------')][:-1]
+    sarcastic = sarcastic[:2000]
+
+for i in range(len(sarcastic)):
+    if sarcastic[i][:1] == '\n':
+        sarcastic[i] = sarcastic[i][1:]
+    sarcastic[i] = sarcastic[i].split('\n---\n')
+sar_len = len(sarcastic)
+
+serious_list2 = set(random.sample(serious_org, sar_len*2))
+serious_list2 = serious_list2 - set(serious_list1)
+serious_list2 = random.sample(list(serious_list2), sar_len)
+
+for i in range(len(serious_list2)):
+    if serious_list2[i][:1] == '\n':
+        serious_list2[i] = serious_list2[i][1:]
+    serious_list2[i] = serious_list2[i].split('\n---\n')
+ser_len = len(serious_list2)
+
+
+sarcastic = np.array(sarcastic)
+serious = np.array(serious_list2)
+
+docs2 = sarcastic.flatten().tolist() + serious.flatten().tolist()
+
+labels = [1] * sar_len + [0] * ser_len
+labels = np.array(labels)
+
+
+sar_temp1 = pad_sequences(token.texts_to_sequences(sarcastic[:, 0]), maxlen=padd_max)
+sar_temp2 = pad_sequences(token.texts_to_sequences(sarcastic[:, 1]), maxlen=padd_max)
+sarcastic_num = np.column_stack((sar_temp1, sar_temp2)).reshape(sar_len, 2, padd_max)
+
+ser_temp1 = pad_sequences(token.texts_to_sequences(serious[:, 0]), maxlen=padd_max)
+ser_temp2 = pad_sequences(token.texts_to_sequences(serious[:, 1]), maxlen=padd_max)
+serious_num = np.column_stack((ser_temp1, ser_temp2)).reshape(ser_len, 2, padd_max)
+
+docs_x2 = np.concatenate((sarcastic_num, serious_num), axis=0)
+print("In-the-wild test accuracy:", model.evaluate(docs_x2, labels)[1])
+
+Y_test2 = labels # Convert one-hot to index
+predict_x=model.predict(docs_x2)
+classes_x=np.round(predict_x)
+print(classification_report(Y_test2, classes_x))
+
+
+import matplotlib.pyplot as plt
+
+y_vloss = history.history['val_loss']
+y_loss = history.history['loss']
+y_acc = history.history['accuracy']
+y_vacc = history.history['val_accuracy']
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(np.arange(len(y_vloss)), y_vloss, marker='.', c='red')
+ax1.plot(np.arange(len(y_loss)), y_loss, marker='.', c='blue')
+ax1.grid()
+plt.setp(ax1, xlabel='epoch', ylabel='loss')
+
+ax2.plot(np.arange(len(y_vacc)), y_vacc, marker='.', c='red')
+ax2.plot(np.arange(len(y_acc)), y_acc, marker='.', c='blue')
+ax2.grid()
+
+plt.setp(ax2, xlabel='epoch', ylabel='accuracy')
+
+plt.show()
---
+++ demo.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import pad_sequences
+
+model1 = load_model("Basic_model.h5")
+with open('Basic_tokenizer.pickle', 'rb') as handle:
+    token1 = pickle.load(handle)
+
+model2 = load_model("LSTM_model.h5")
+with open('LSTM_tokenizer.pickle', 'rb') as handle:
+    token2 = pickle.load(handle)
+
+st.title("Sarcasm Detection Model")
+st.write("Enter a title with a reply and I'll tell you something interesting:")
+
+input_1 = st.text_input("Title of Post")
+input_2 = st.text_input("Comment Text")
+
+padd_max = 100
+
+def predict1(input_1, input_2):
+    #print(input_1, input_2)
+    to_pred = [[input_1, input_2]]
+    to_pred = np.array(to_pred)
+    print(to_pred)
+    to_pred_temp1 = pad_sequences(token1.texts_to_sequences(to_pred[:, 0]), maxlen=padd_max)
+    to_pred_temp2 = pad_sequences(token1.texts_to_sequences(to_pred[:, 1]), maxlen=padd_max)
+    to_pred_num = np.column_stack((to_pred_temp1, to_pred_temp2)).reshape(1, 2, padd_max)
+
+    prediction = model1.predict(to_pred_num)
+    if prediction[0][0] < 0.5:
+        return "### Basic model is ***:blue[" + str(round(prediction[0][0]*100, 2)) + "\%]*** sure that was sarcastic"
+    return "### Basic model is ***:red[" + str(round(prediction[0][0]*100, 2)) + "\%]*** sure that was sarcastic"
+
+def predict2(input_1, input_2):
+    #print(input_1, input_2)
+    to_pred = [[input_1, input_2]]
+    to_pred = np.array(to_pred)
+    print(to_pred)
+    to_pred_temp1 = pad_sequences(token2.texts_to_sequences(to_pred[:, 0]), maxlen=padd_max)
+    to_pred_temp2 = pad_sequences(token2.texts_to_sequences(to_pred[:, 1]), maxlen=padd_max)
+    to_pred_num = np.column_stack((to_pred_temp1, to_pred_temp2)).reshape(1, 2, padd_max)
+
+    prediction = model2.predict(to_pred_num)
+    if prediction[0][0] < 0.5:
+        return "### LSTM model is ***:blue[" + str(round(prediction[0][0]*100, 2)) + "\%]*** sure that was sarcastic"
+    return "### LSTM model is ***:red[" + str(round(prediction[0][0]*100, 2)) + "\%]*** sure that was sarcastic"
+
+if st.button("Get prediction"):
+    st.markdown(predict1(input_1, input_2))
+    st.markdown(predict2(input_1, input_2))
+
+st.write("Examples:")
+st.markdown("> High crime in Republican cities fueled by guns, inequality")
+st.markdown("- Who would have thought that making it easy to access firearms would result in more people shooting each other? Wild!")
+st.markdown("- The high crime rate in Dem controlled cities is also fueled by guns. Most of their crime is within demographic segments, so I’m not sure inequality is the issue there.")
+st.markdown("- Woah, you mean crime is perpetrated by people who are desperate, poor, hungry, and carrying deadly weapons? What a novel concept!")
+st.markdown("- What a grossly misleading title. There is nothing in the article about guns. It says social safety nets reduce poverty, which in turn reduces violent crimes.")
